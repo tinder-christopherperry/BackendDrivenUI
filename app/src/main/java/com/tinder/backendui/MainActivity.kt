a97c64ce24e04a1d0f134749274a3a74bf9de67e
tinder-christopherperry <christopher.perry@gotinder.com>
2021-03-02 15:44:51 -0800

    Make carousel items shown configurable

--- app/src/main/java/com/tinder/backendui/MainActivity.kt
+++ app/src/main/java/com/tinder/backendui/MainActivity.kt
@@ -2,15 +2,16 @@ package com.tinder.backendui
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.ViewModelProvider
 import com.tinder.plugins.ComposableBuilder
 import com.tinder.plugins.Content
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,11 +22,10 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var componentProviders: Map<String, @JvmSuppressWildcards ComposableBuilder>
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         val state = viewModel.getState()
         if (state is State.Success) {
@@ -41,7 +41,10 @@ class MainActivity : AppCompatActivity() {
             items(response.components) { component ->
                 when (component.type) {
                     "Carousel" -> {
-                        Carousel(component.content as Content.CarouselContent)
+                        Carousel(
+                            content = component.content as Content.CarouselContent,
+                            numCarouselItemsShown = NUM_CAROUSEL_ITEMS_SHOWN
+                        )
                     }
                     else -> {
                         componentProviders[component.type]!!.BuildComposable(component)
@@ -52,11 +55,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     @Composable
-    private fun Carousel(content: Content.CarouselContent) {
+    private fun Carousel(content: Content.CarouselContent, numCarouselItemsShown: Float) {
+        require(numCarouselItemsShown > 0f)
         LazyRow {
             items(content.items) { item ->
-                componentProviders[item.type]!!.BuildComposable(item)
+                Box(Modifier.fillParentMaxWidth(1f / numCarouselItemsShown)) {
+                    componentProviders[item.type]!!.BuildComposable(item)
+                }
             }
         }
+    }
+
+    companion object {
+        private const val NUM_CAROUSEL_ITEMS_SHOWN = 1.25f
     }
 }
